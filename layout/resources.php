3c62055a5cdc88537fd73334a6e3070ad4599053
Daniel Moulson <daniel@documobi.com>
2016-02-02 10:29:13 +0000

    resources

--- layout/resources.php
+++ layout/resources.php
@@ -3,4 +3,9 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 
 <!-- Load Bootstrap -->
+<link rel="stylesheet" href="../resources/css/bootstrap.min.css" type="text/css" media="screen">
+<script src="../resources/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="../resources/js/npm.js" type="text/javascript" charset="utf-8"></script>
 
+<!-- Load TinyMCE -->
+<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
